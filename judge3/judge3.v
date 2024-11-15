7e355efafbbeae5014391f664584fcb26a4bec67
xielianqin <1323229802@qq.com>
2024-11-16 00:33:05 +0800

    judge

--- judge3/judge3.v
+++ judge3/judge3.v
@@ -1,8 +1,30 @@
-module judge3(          //当多数人同意时输出1
-    input a,b,c,
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/16 00:27:09
+// Design Name: 
+// Module Name: judge
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module judge3(
+    input a, b, c,
     output out
 );
-    
-    assign out = // 超过两个输入1
-    
+
+    assign out = (a & b) | (a & c) | (b & c);
+
 endmodule
--- judge3/judge3_tb.v
+++ judge3/judge3_tb.v
@@ -1,16 +1,61 @@
-/*
-testbench激励文件
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/16 00:28:13
+// Design Name: 
+// Module Name: tb_judge
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-测试点应至少包括：
-...
-{a,b,c} = {0,0,0};
-#10
-{a,b,c} = {0,0,1};
-#10
-{a,b,c} = {0,1,1};
-#10
-{a,b,c} = {1,1,1};
-#10
-$finish;
 
-*/
+module tb_judge3;
+
+    // Inputs
+    reg a;
+    reg b;
+    reg c;
+
+    // Output
+    wire out;
+
+    // Instantiate the Unit Under Test (UUT)
+    judge3 uut (
+        .a(a), 
+        .b(b), 
+        .c(c), 
+        .out(out)
+    );
+
+    initial begin
+        // Initialize Inputs
+        a = 0; b = 0; c = 0;
+        #10; // Wait for 10 time units
+        a = 0; b = 0; c = 1;
+        #10;
+        a = 0; b = 1; c = 0;
+        #10;
+        a = 0; b = 1; c = 1;
+        #10;
+        a = 1; b = 0; c = 0;
+        #10;
+        a = 1; b = 0; c = 1;
+        #10;
+        a = 1; b = 1; c = 0;
+        #10;
+        a = 1; b = 1; c = 1;
+        #10;
+    end
+      
+endmodule
