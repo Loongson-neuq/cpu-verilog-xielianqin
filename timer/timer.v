ec33192cbae7f8de76b858b53956346e21b455fa
xielianqin <1323229802@qq.com>
2024-11-16 00:13:44 +0800

    timer

--- timer/timer.v
+++ timer/timer.v
@@ -1,10 +1,32 @@
-module timer(                   // 每个时钟周期自增1
-	input clk,
-    output [7:0] timer
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/16 00:06:13
+// Design Name: 
+// Module Name: timer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module timer(
+    input clk,
+    output reg [7:0] timer
 );
-    
+
     always @ (posedge clk) begin
-    	...... 
+        timer <= timer + 1;
     end
-    
+
 endmodule
--- timer/timer_tb.v
+++
@@ -1,7 +0,0 @@
-/*
-testbench激励文件
-
-时钟信号：
-always #5 clk = ~clk;
-
-*/
